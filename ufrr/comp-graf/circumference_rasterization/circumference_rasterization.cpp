81a62df065e42bca6c0296a54a5cfb59dbe20f55
George Z <georgezambonin@gmail.com>
2023-10-28 07:40:45 -0400

    comp-graf: circumference rasterization fix

--- ufrr/comp-graf/circumference_rasterization/circumference_rasterization.cpp
+++ ufrr/comp-graf/circumference_rasterization/circumference_rasterization.cpp
@@ -10,16 +10,17 @@ void cr::CircumferenceRasterization::parametric_equation(int x_c, int y_c, int r
 
     if (x_c + radius >= cols || x_c - radius < 0 || y_c + radius >= rows || y_c - radius < 0){ // enchant it
         throw std::invalid_argument("Circumference out of bound.");
+        return;
     }
     
     int t;
     float x, y, t_radians;
-    float pi_180 = PI / 180.f;
+    float pi_180 = (float) PI / 180.f;
 
     for(t = 1; t <= 360; t++) {
         t_radians = (float) t * pi_180;
         x = x_c + radius * cosf(t_radians);
         y = y_c + radius * sinf(t_radians);
-        frame_buffer.data[(int)y][(int)x] = 1;
+        frame_buffer.data[(int)round(y)][(int)round(x)] = 1;
     }
 }
