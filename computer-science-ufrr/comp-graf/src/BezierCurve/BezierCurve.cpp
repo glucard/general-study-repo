8715e3217fb07e4d7766dde98d5942d3ba76a036
George Z <georgezambonin@gmail.com>
2023-11-04 09:10:19 -0400

    comp-graf: belziercurve init and polygon cmake fix

---
+++ computer-science-ufrr/comp-graf/src/BezierCurve/BezierCurve.cpp
@@ -0,0 +1,1 @@
+#include "BezierCurve.hpp"
---
+++ computer-science-ufrr/comp-graf/src/BezierCurve/BezierCurve.hpp
@@ -0,0 +1,19 @@
+#ifndef BEZIER_CURVE_HPP
+#define BEZIER_CURVE_HPP
+
+#include <iostream>
+#include "array2d.hpp"
+#include "Polygon.hpp"
+
+namespace bzc
+{
+    class BezierCurve {
+    private:
+
+    public:
+        void parametric();
+    };
+} 
+
+
+#endif
